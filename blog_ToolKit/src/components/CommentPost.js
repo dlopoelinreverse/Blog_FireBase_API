655faf0f94acc434cb819369f5574a057b0f6ccc
dlopoelinreverse <l.lesaul@gmail.com>
2022-11-30 16:40:17 +0100

    Comments

--- blog_ToolKit/src/components/CommentPost.js
+++ blog_ToolKit/src/components/CommentPost.js
@@ -1,18 +1,47 @@
 import React from "react";
-
-import { useState } from "react";
-import { auth } from "../utils/firebase.config";
+import { useRef } from "react";
+import { doc, updateDoc } from "firebase/firestore";
+import { auth, db } from "../utils/firebase.config";
 
 const CommentPost = ({ post, user }) => {
-  const handleComment = () => {};
+  const answerContent = useRef();
+  console.log(post.comments);
+  const handleComment = (e) => {
+    e.preventDefault();
+
+    let data = [];
+
+    if (post.comments === null) {
+      data = [
+        {
+          commentAuthor: user.displayName,
+          text: answerContent.current.value,
+        },
+      ];
+    } else {
+      data = [
+        ...post.comments,
+        {
+          commentAuthor: user.displayName,
+          text: answerContent.current.value,
+        },
+      ];
+    }
+
+    updateDoc(doc(db, "posts", post.id), { comments: data });
+    answerContent.current.value = "";
+  };
   return (
     <div className="comment-container">
       <h5 className="comment-title">Commentaires</h5>
       {/* MAP */}
 
       {user ? (
-        <form onSubmit={(e) => handleComment()}>
-          <textarea placeholder="Envoyer un commentaire"></textarea>
+        <form onSubmit={(e) => handleComment(e)}>
+          <textarea
+            placeholder="Envoyer un commentaire"
+            ref={answerContent}
+          ></textarea>
           <input type="submit" />
         </form>
       ) : (
